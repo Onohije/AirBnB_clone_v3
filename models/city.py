fe3f53220786e37e9cf36878862abb74976edbda
fessycode <fessydoris@gmail.com>
2023-11-01 02:43:47 +0000

    update

--- models/city.py
+++ models/city.py
@@ -14,10 +14,25 @@ class City(BaseModel, Base):
         __tablename__ = 'cities'
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         name = Column(String(128), nullable=False)
-        places = relationship("Place", backref="cities")
+        places = relationship("Place", backref="cities",
+                              cascade='all, delete, delete-orphan')
     else:
         state_id = ""
         name = ""
+
+        @property
+        def places(self):
+            """Returns a list of places within a city"""
+            from models.place import Place
+            from models import storage
+
+            resp = []
+
+            for place in storage.all(Place).values():
+                if place.city_id == self.id:
+                    resp.append(place)
+
+            return resp
 
     def __init__(self, *args, **kwargs):
         """initializes city"""
